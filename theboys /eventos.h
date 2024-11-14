0a1acb5259cd5190c5ef3dfe03ac0615fa491abe
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2024-11-14 17:14:28 -0300

    Create eventos.h

---
+++ theboys /eventos.h
@@ -0,0 +1,19 @@
+void chega(int t, int h, int b);
+
+void espera(int t, int h, int b);
+
+void desiste(int t, int h, int b);
+
+void avisa(int t, int b);
+
+void entra(int t, int h, int b);
+
+void sai(int t, int h, int b);
+
+void viaja( int t, int h, int d);
+
+void morre(int t, int h, int b);
+
+void missao(int t, int m);
+
+void fim(int t);
