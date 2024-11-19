67fa515c4ac218becf9cf80ddef6c96ae0e17ee0
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2024-11-19 14:54:27 -0300

    Update eventos.c

--- theboys /eventos.c
+++ theboys /eventos.c
@@ -1,66 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "conjunto.h"
 #include "fila_espera.h"
+#include "entidades.h"
 
-void chega(int t, int h, int b)
+void chega(int t, struct heroi_t *H, struct base_t *B)
 {
-    /*atualiza base de H
-
-se há vagas em B e a fila de espera em B está vazia:
-    espera = true
-senão:
-    espera = (paciência de H) > (10 * tamanho da fila em B)
-
-se espera:
-    cria e insere na LEF o evento ESPERA (agora, H, B)
-senão:
+    int espera = 0;
+    H->base = B->id; //atualiza base de H
+    if ((cjto_card(B->pres)<B->lotação) && (fila_tamanho(B->espera)==0) ) // se há vagas em B e a fila de espera em B está vazia:
+        espera = 1; // espera = true
+    else // senão:
+        if (H->paciencia > (10 * fila_tamanho(B->espera))) // espera = (paciência de H) > (10 * tamanho da fila em B)
+            espera = 1;
+    
+    if (espera) // se espera:
+    /*cria e insere na LEF o evento ESPERA (agora, H, B)
+    senão:
     cria e insere na LEF o evento DESISTE (agora, H, B)*/
 }
 
-void espera(int t, int h, int b){
+void espera(int t, struct heroi_t *H, struct base_t *B)
+{
    /* adiciona H ao fim da fila de espera de B
 cria e insere na LEF o evento AVISA (agora, B)*/
 }
 
-void desiste(int t, int h, int b){
+void desiste(int t, struct heroi_t *H, struct base_t *B)
+{
    /* escolhe uma base destino D aleatória
 cria e insere na LEF o evento VIAJA (agora, H, D)*/
 }
 
-void avisa(int t, int b){
+void avisa(int t, struct base_t *B)
+{
    /*enquanto houver vaga em B e houver heróis esperando na fila:
     retira primeiro herói (H') da fila de B
     adiciona H' ao conjunto de heróis presentes em B
     cria e insere na LEF o evento ENTRA (agora, H', B)*/
 }
 
-void entra(int t, int h, int b){
+void entra(int t, struct heroi_t *H, struct base_t *B)
+{
 /*calcula TPB = tempo de permanência na base:
     TPB = 15 + paciência de H * aleatório [1...20]
 cria e insere na LEF o evento SAI (agora + TPB, H, B)*/
 }
 
-void sai(int t, int h, int b){
+void sai(int t, struct heroi_t *H, struct base_t *B)
+{
     /*retira H do conjunto de heróis presentes em B
 escolhe uma base destino D aleatória
 cria e insere na LEF o evento VIAJA (agora, H, D)
 cria e insere na LEF o evento AVISA (agora, B)*/
 }
 
-void viaja( int t, int h, int d){
+void viaja(int t, struct heroi_t *H, struct base_t *B)
+{
     /*calcula duração da viagem:
     distância = distância cartesiana entre a base atual de H e a base D 
     duração = distância / velocidade de H
 cria e insere na LEF o evento CHEGA (agora + duração, H, D)*/
 }
 
-void morre(int t, int h, int b){
+void morre(int t, struct heroi_t *H, struct base_t *B)
+{
     /*retira H do conjunto de heróis presentes em B
 muda o status de H para morto 
 cria e insere na LEF o evento AVISA (agora, B)*/
 }
 
-void missao(int t, int m){
+void missao(int t, int m)
+{
 /*calcula a distância de cada base ao local da missão M
 encontra BMP = base mais próxima da missão cujos heróis possam cumpri-la 
 se houver uma BMP:
@@ -75,7 +86,8 @@ senão:
     cria e insere na LEF o evento MISSAO (T + 24*60, M) para o dia seguinte*/
 }
 
-void fim(int t){
+void fim(int t)
+{
     /*encerra a simulação
   apresenta estatísticas dos heróis
   apresenta estatísticas das bases
