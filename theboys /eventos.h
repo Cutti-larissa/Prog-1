569f2c9b256315ada8b33325dd209669a7ddd4ed
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2024-12-04 17:05:41 -0300

    Update eventos.h

--- theboys /eventos.h
+++ theboys /eventos.h
@@ -2,32 +2,43 @@
 #define EVENTOS
 
 /*tipo 0*/
-void chega(int t, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void chega(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 1*/
-void espera(int t, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void espera(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 2*/
-void desiste(int t, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void desiste(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 3*/
-void avisa(int t, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void avisa(int tempo, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 4*/
-void entra(int t, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void entra(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 5*/
-void sai(int t, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void sai(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 6*/
-void viaja(int t, struct heroi_t *H, struct base_t *D, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void viaja(int tempo, struct heroi_t *H, struct base_t *D, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 7*/
-void morre(int t, struct heroi_t *H, struct base_t *B, int M, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void morre(int tempo, struct heroi_t *H, struct base_t *B, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 8*/
-void missao(int t, struct ev_t *M, struct fprio_t *LEF, struct mundo_t *W);
+/**/
+void missao(int tempo, struct ev_t *M, struct fprio_t *LEF, struct mundo_t *W);
 
 /*tipo 9*/
+/**/
 void fim(struct mundo_t *W, int event);
+
 #endif
