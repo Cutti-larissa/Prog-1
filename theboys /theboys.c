9aee0138fee320801488a32d7fc026a73065771d
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2024-12-04 15:35:56 -0300

    Update theboys.c

--- theboys /theboys.c
+++ theboys /theboys.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "conjunto.h"
 #include "fprio.h"
 #include "lista.h"
@@ -12,17 +13,22 @@
 void inicia_mundo(struct mundo_t *W)
 {
   W->nHerois = N_HEROIS;
-  W->Herois[N_HEROIS] = calloc(N_HEROIS, sizeof (struct heroi_t *));
+  for (int i = 0; i < N_HEROIS; i++) {
+    W->Herois[i] = NULL; //inicializa vetor herois
+  }
   W->nBases = N_BASES;
-  W->Bases[N_BASES] = calloc(N_BASES, sizeof (struct base_t *));
+  for (int i = 0; i < N_BASES; i++) {
+    W->Bases[i] = NULL; //inicializa vetor bases
+  }
   W->nMissoes = N_MISSOES;
   W->mCumpridas = 0;
-  W->Missoes[N_MISSOES] = calloc(N_MISSOES, sizeof (struct mission_t *));
+  for (int i = 0; i < N_MISSOES; i++) {
+    W->Missoes[i] = NULL; //inicializa vetor missoes
+  }
   W->nHabilidades = N_HABILIDADES;
   W->tam_mundo.x = N_TAMANHO_MUNDO;
   W->tam_mundo.y = N_TAMANHO_MUNDO;
   W->relogio = T_INICIO;
-  printf("Mundo criado\n");
 }
 
 void inicia_herois(struct mundo_t *W)
@@ -53,7 +59,6 @@ void inicia_herois(struct mundo_t *W)
     heroi->hab = habi; //habilidades = conjunto com tamanho aleatório [1...3] de habilidades aleatórias
     W->Herois[i] = heroi;
   }
-  printf("Herois criados\n");
 }
 
 void inicia_bases(struct mundo_t *W)
@@ -64,27 +69,19 @@ void inicia_bases(struct mundo_t *W)
     struct base_t *base = malloc (sizeof(struct base_t));
     struct cjto_t *heros = cjto_cria(N_HEROIS); //conjunto que pode armazenar valores de 0 até N_HEROIS - 1
     struct lista_t *esp = lista_cria();
-    if (!base || !heros || !esp){
-      printf("Erro ao tentar alocar memória\n");
-      return;}
+    if (!base || !heros || !esp)
+      return;
     
     base->id = i; //id = número sequencial [0...N_BASES-1]
-    base->local = malloc(sizeof(struct coord_t));
-    if (!base->local){
-      free(base);
-      cjto_destroi(heros);
-      lista_destroi(esp);
-      return;}
-    base->local->x = rand() % (N_TAMANHO_MUNDO); //local = par de números aleatórios [0...N_TAMANHO_MUNDO-1]
-    base->local->y = rand() % (N_TAMANHO_MUNDO); // ou usar W->tam_mundo
+    base->local.x = rand() % (N_TAMANHO_MUNDO); //local = par de números aleatórios [0...N_TAMANHO_MUNDO-1]
+    base->local.y = rand() % (N_TAMANHO_MUNDO); // ou usar W->tam_mundo
     base->max = rand() % (10 - 3 + 1) + 3; //lotação = número aleatório [3...10]
     base->pres = heros; // presentes = conjunto vazio (com capacidade para armazenar IDs de heróis até a lotação da base) (?)
     base->espera = esp;
+    base->fila_max = 0;
     base->nMissoes = 0; 
-    base->filamax = 0;
     W->Bases[i] = base;  
   }
-  printf("Bases criadas\n");
 }
 void inicia_missoes(struct mundo_t *W)
 {
@@ -99,11 +96,8 @@ void inicia_missoes(struct mundo_t *W)
       return;
     
     missao->id = i; //id = número sequencial [0...N_MISSOES-1]
-    missao->local = malloc(sizeof(struct coord_t));
-    if (!missao->local)
-      return;
-    missao->local->x = rand() % (N_TAMANHO_MUNDO); //local = par de números aleatórios [0...N_TAMANHO_MUNDO-1]
-    missao->local->y = rand() % (N_TAMANHO_MUNDO);
+    missao->local.x = rand() % (N_TAMANHO_MUNDO); //local = par de números aleatórios [0...N_TAMANHO_MUNDO-1]
+    missao->local.y = rand() % (N_TAMANHO_MUNDO);
     habis = rand() % (10 - 6 + 1) + 6;
     for (int j = 0; j<habis; j++){
       int h = rand () % (N_HABILIDADES);
@@ -117,7 +111,6 @@ void inicia_missoes(struct mundo_t *W)
     missao->tentativas = 0;
     W->Missoes[i] = missao;
   }
-  printf("Criou missões\n");
 }
 
 void inicia_eventos(struct mundo_t *W, struct fprio_t *LEF)
@@ -148,7 +141,6 @@ void inicia_eventos(struct mundo_t *W, struct fprio_t *LEF)
   if (!fim)
     return;
   fprio_insere(LEF,fim,9,tempo); //criar e inserir na LEF o evento FIM (tempo)
-  printf("Eventos iniciados\n");
 }
 
 void destroi_mundo(struct mundo_t *W)
@@ -159,43 +151,37 @@ void destroi_mundo(struct mundo_t *W)
     cjto_destroi(W->Herois[i]->hab);
     free(W->Herois[i]);
   }
-  free(W->Herois[N_HEROIS]);
   for (int i=0; i<N_BASES; i++){
     cjto_destroi(W->Bases[i]->pres);
     lista_destroi(W->Bases[i]->espera);
     free(W->Bases[i]);
   }
-  free(W->Bases[N_BASES]);
   for (int i=0; i<N_MISSOES; i++){
     cjto_destroi(W->Missoes[i]->hab);
     free(W->Missoes[i]);
   }
-  free(W->Missoes[N_MISSOES]);
   free(W);
 }
 
 // programa principal
 int main ()
 {
-  printf("Começando testes\n");
-  int tipo_ev, tempo, evt_trat;
+  srand(time(NULL));
+  int tipo_ev, tempo, evt_trat = 0;
   struct ev_t *ev;
   struct mundo_t *W = malloc(sizeof(struct mundo_t)); //iniciar as entidades e atributos do mundo
   if (!W)
     return (-1);
-  printf("W criado\n");
   struct fprio_t *LEF = fprio_cria(); //criar lista de eventos futuros
   if (!LEF){
     free(W);
     return (-1);}
-  printf("LEF criado\n");
-
+  
   inicia_mundo(W);
   inicia_herois(W);
   inicia_bases(W);
   inicia_missoes(W);
   inicia_eventos(W,LEF); //criar os eventos iniciais
-  evt_trat = 0;
 
   while (W->relogio < T_FIM_DO_MUNDO){ //repetir até o fim da simulação //laço da simulação
     ev = fprio_retira(LEF, &tipo_ev, &tempo); //ev = 1º evento da lista de eventos futuros
