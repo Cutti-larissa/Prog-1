9c7754901a1b56890ec0af564a3330d6a4e04c69
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2025-05-30 14:58:46 -0300

    Update lista.c

--- tp4/lista.c
+++ tp4/lista.c
@@ -2,150 +2,159 @@
 #include <stdlib.h> 
 #include "lista.h"
 
-struct lista_t *lista_cria (){
-  struct lista_t *lst = malloc(sizeof(struct lista_t));
+struct lista_t* lista_cria (){
+  struct lista_t* lst = malloc(sizeof(struct lista_t));
   if (!lst)
     return(NULL);
+  
   lst->prim=NULL;
   lst->ult=NULL;
   lst->tamanho=0;
+  
   return(lst);
 }
 
-int lista_insere (struct lista_t *lst, int item, int pos){
-  int i;
-  struct item_t *itm = malloc(sizeof(struct item_t));
-  if(!lst){
-    free(itm);
+int lista_insere (struct lista_t* lst, int item, int pos){
+  if(!lst)
     return(-1);
-  }
-  if (!itm || pos<-1)
+  
+  struct item_t* itm = malloc(sizeof(struct item_t));
+  if (!itm || pos < -1)
     return(-1);
+  
   itm->valor = item;
+  
   if (pos > lst->tamanho || pos == -1){
-    itm->ant=lst->ult;
-    itm->prox=NULL;
+    itm->ant = lst->ult;
+    itm->prox = NULL;
     if (lst->tamanho == 0)
       lst->prim=itm;
     if(lst->ult)
-      lst->ult->prox=itm;
+      lst->ult->prox = itm;
     lst->ult = itm;
   }else{
     if(lst->tamanho==0){
-      itm->ant=NULL;
-      itm->prox=NULL;
-      lst->ult=itm;
+      itm->ant = NULL;
+      itm->prox = NULL;
+      lst->ult = itm;
     }else{
-      struct item_t *aux;
-      aux=lst->prim;
-      for(i=0;i<pos;i++)
-        aux=aux->prox;
-      itm->ant=aux->ant;
-      itm->prox=aux;
-      if(pos!=0)
-        aux->ant->prox=itm;
-      aux->ant=itm;
+      struct item_t* aux;
+      aux = lst->prim;
+      for(int i = 0; i < pos; i++)
+        aux = aux->prox;
+      itm->ant = aux->ant;
+      itm->prox = aux;
+      if(pos != 0)
+        aux->ant->prox = itm;
+      aux->ant = itm;
     }
   }
-  if(pos==0)
-    lst->prim=itm;
+  if(pos == 0)
+    lst->prim = itm;
   lst->tamanho++;
+  
   return(lst->tamanho);
 }
 
-int lista_retira (struct lista_t *lst, int *item, int pos){
-  int i;
-  struct item_t *aux;
+int lista_retira (struct lista_t* lst, int* item, int pos){
   if(!lst)
     return(-1);
+  
   if (!item || lst->tamanho == 0 || pos<-1 || pos>lst->tamanho)
     return(-1);
-  if (pos == -1 || pos == lst->tamanho-1){
+
+  struct item_t* aux = NULL;
+  if (pos == -1 || pos == lst->tamanho - 1)
+  {
     *item = lst->ult->valor;
     aux = lst->ult;
-    if (lst->tamanho==1){
-      lst->prim=NULL;
-      lst->ult=NULL;
+    if (lst->tamanho == 1){
+      lst->prim = NULL;
+      lst->ult = NULL;
     }else{
       lst->ult->ant->prox = NULL;
-      lst->ult=lst->ult->ant;
+      lst->ult = lst->ult->ant;
     }
-  }else{
-    aux=lst->prim;
-    for (i=0;i<pos;i++)
-      aux=aux->prox;
+  }else
+  {
+    aux = lst->prim;
+    for (int i = 0; i < pos; i++)
+      aux = aux->prox;
     *item = aux->valor;
-    if(pos==0){
-      aux->prox->ant=NULL;
-      lst->prim=aux->prox;
+    if(pos == 0){
+      aux->prox->ant = NULL;
+      lst->prim = aux->prox;
     }else{
-      aux->ant->prox=aux->prox;
-      aux->prox->ant=aux->ant;
+      aux->ant->prox = aux->prox;
+      aux->prox->ant = aux->ant;
     }
   }
+  
   free(aux);
   lst->tamanho--;
+  
   return(lst->tamanho);
 }
 
-int lista_consulta (struct lista_t *lst, int *item, int pos){
-  int i;
-  struct item_t *aux;
+int lista_consulta (struct lista_t* lst, int* item, int pos){
   if(!lst)
     return(-1);
-  if (!item || pos>=lst->tamanho || pos <-1 || lst->tamanho == 0)
+  
+  if (!item || pos >= lst->tamanho || pos < -1 || lst->tamanho == 0)
     return(-1);
+  
   if(pos == -1){
-    *item=lst->ult->valor;
+    *item = lst->ult->valor;
     return(lst->tamanho);
   }
-  aux=lst->prim;
-  for (i=1;i<=pos;i++)
-    aux=aux->prox;
-  *item=aux->valor;
+  
+  struct item_t* aux = lst->prim;
+  for (int i = 1; i <= pos; i++)
+    aux = aux->prox;
+  
+  *item = aux->valor;
+  
   return(lst->tamanho);
 }
 
-int lista_procura (struct lista_t *lst, int valor){
-  int i;
-  struct item_t *aux;
+int lista_procura (struct lista_t* lst, int valor){
   if(!lst)
     return(-1);
-  aux=lst->prim;
-  for(i=1;i<=lst->tamanho;i++){
-    if(aux->valor==valor)
+  
+  struct item_t* aux = lst->prim;
+  for(int i = 1; i <= lst->tamanho; i++){
+    if(aux->valor == valor)
       return(i-1);
-    aux=aux->prox;
+    aux = aux->prox;
   }
+  
   return(-1);
 }
 
-int lista_tamanho (struct lista_t *lst){
+int lista_tamanho (struct lista_t* lst){
   if(!lst)
     return(-1);
   return(lst->tamanho);
 }
 
-void lista_imprime (struct lista_t *lst){
-  int i;
-  struct item_t *aux;
-  aux=lst->prim;
-  for (i=1;i<=lst->tamanho;i++){
+void lista_imprime (struct lista_t* lst){
+  struct item_t* aux = lst->prim;
+  for (int i = 1; i <= lst->tamanho; i++){
     printf("%d",aux->valor);
-    if (i!=lst->tamanho)
+    if (i != lst->tamanho)
       printf(" ");
-    aux=aux->prox;
+    aux = aux->prox;
   }
 }
 
-struct lista_t *lista_destroi (struct lista_t *lst){
-  struct item_t *aux;
-  aux=lst->prim;
-  while(lst->prim != NULL) {
-    lst->prim=aux->prox;
+struct lista_t* lista_destroi (struct lista_t* lst){
+  struct item_t* aux = lst->prim;
+  while(lst->prim != NULL){
+    lst->prim = aux->prox;
     free(aux);
-    aux=lst->prim;
+    aux = lst->prim;
   }
   free(lst);
+  
   return(NULL);
 }
