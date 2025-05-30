e97c91778c227305efdd7dd957bd09b817c15b75
Larissa Cutti <162053440+Cutti-larissa@users.noreply.github.com>
2025-05-30 14:40:54 -0300

    Update racional.c

--- tp1/racional.c
+++ tp1/racional.c
@@ -7,105 +7,75 @@
  * foram definidos em racional.h. Neste arquivo também podem ser definidas
  * funções auxiliares para facilitar a implementação daquelas funções. */
 
-/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
 #include <stdlib.h>
 #include "racional.h"
 
-/* Retorna um número aleatório entre min e max, inclusive. */
 long aleat (long min, long max){
-     return((rand() % (2*max + 1)) + min);    //(2*max + 1) separa o intervalo e (+min) desloca ele
+     return((rand() % (2*max + 1)) + min);
 }
 
-/* Atribui os valores de numerador e denominador indicados a um racional */
 struct racional cria_r (long numerador, long denominador){
-     struct racional r;
-     
+     struct racional r;     
      r.num = numerador;
      r.den = denominador;
-     
      return(r);
 }
 
-/* Verifica se o racional é válido, ou seja, se o denominador não é 0 */
 int valido_r (struct racional r){
      if(r.den == 0)
-          return(0);
-     
+          return(0);     
      return(1);
 }
 
-/* Retira o sinal do número para que a 
-* conta do mdc possa ser realizada */
 long modulo (long a){
-     if (a<0){
+     if (a<0)
           a = (a * (-1));
-     }
-     
      return(a);
 }
 
-/* calcula o Máximo Divisor Comum pelo método de Euclides */
 long mdc (long a, long b){
-     long sobra;
-
-     if ((a==0) | (b==0)){ //testa se não irá dividir por 0 no método
+     if ((a==0) | (b==0)) 
           return(1);
-     }
      
-     sobra = 1;
+     long sobra = 1;
      
      while (sobra != 0){
           sobra = a % b;
           a = b;
-          if (sobra != 0){
+          if (sobra != 0)
                b = sobra;
-          }
      }
-     
      return(b);
 }
 
-/* Recebe um número racional e o simplifica, caso seja inválido
-* devolve o racional sem simplificar */
 struct racional simplifica_r (struct racional r){
-     int div;
-     
      if (!valido_r(r))
           return(r);
      
-     /*desloca o sinal de negativo para o numerador, 
-     * caso o denominador seja negativo, 
-     * caso os dois sejam negativos retornará um positivo*/
      if(r.den<0){    
           r.num = (r.num * (-1));
           r.den = (r.den * (-1));
      }
      
-     div = mdc(modulo(r.num),modulo(r.den)); //acha o mdc para reduzir ao máximo os valores
+     int div = mdc(modulo(r.num),modulo(r.den)); 
      r.num = r.num / div;
      r.den = r.den / div;
      
      return(r);
 }
 
-/* Retorna um número racional aleatório na forma simplificada */
 struct racional sorteia_r (long min, long max){
-     long numerador,denominador;
-     
-     numerador = aleat(min,max);
-     denominador = aleat(min,max);
+     long numerador = aleat(min,max);
+     long denominador = aleat(min,max);
      
      return(simplifica_r(cria_r(numerador,denominador)));
-     /* Seria possível remover o simplifica dessa linha, porém foi deixado para ficar de acordo com as especificações do .h*/
 }
 
-/* Mínimo Múltiplo Comum entre a e b */
 long mmc (long a, long b){
      return((a*b)/ mdc(a,b));
 }
 
-/*Retorna a soma de dois racionais r1 e r2*/
 struct racional soma_r (struct racional r1, struct racional r2){
      struct racional soma_t;
      
@@ -115,7 +85,6 @@ struct racional soma_r (struct racional r1, struct racional r2){
      return(soma_t);
 }
 
-/*Retorna a subtração de dois racionais r1 e r2*/
 struct racional subtrai_r (struct racional r1, struct racional r2){
      struct racional subtrai_t;
      
@@ -125,7 +94,6 @@ struct racional subtrai_r (struct racional r1, struct racional r2){
      return(subtrai_t);
 }
 
-/*Retorna a multiplicação de dois racionais r1 e r2*/
 struct racional multiplica_r (struct racional r1, struct racional r2){
      struct racional multiplica_t;
 
@@ -135,40 +103,28 @@ struct racional multiplica_r (struct racional r1, struct racional r2){
      return(multiplica_t);
 }
 
-/* Inverte o numerador e o denominador de um racional
-* para realizar a divisão pelo método de multiplicação pelo inverso */
 struct racional inverte_r (struct racional r){
-     int aux;
-     
-     aux = r.num;
+     int aux = r.num;
      r.num = r.den;
      r.den = aux;
      
      return(r);
 }
 
-/*Retorna a divisão de dois racionais r1 e r2*/
 struct racional divide_r (struct racional r1, struct racional r2){
-     struct racional divide_t;
-     
-     divide_t = multiplica_r(r1,inverte_r(r2));
-     
+     struct racional divide_t = multiplica_r(r1,inverte_r(r2));
      return(divide_t);
 }
 
-/* Imprime um racional simplificado */
 void imprime_r (struct racional r){
      r = simplifica_r(r);
      
-     if (r.num == 0) 
+     if (r.num == 0){ 
           printf("0 ");
-     else if (r.den == 1){
+     }else if (r.den == 1){
           printf("%ld ", r.num);
-     }
-     else if (r.num == r.den){
+     }else if (r.num == r.den){
           printf("1 ");
-     }
-     else{
+     }else
           printf("%ld/%ld ", r.num,r.den);
-     }
 }
